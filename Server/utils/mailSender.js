03e6a5f104e9aec69f36661e17f072cc0daaa36e
Ujjwal092 <us581318@gmail.com>
2026-03-28 22:02:22 +0530

    commit

--- Server/utils/mailSender.js
+++ Server/utils/mailSender.js
@@ -3,19 +3,20 @@ const nodemailer = require("nodemailer");
 const mailSender = async (email, title, body) => {
   try {
     let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: 587, //  important
-      secure: false, //  important
+      host: "smtp-relay.brevo.com",
+      port: 587, //  MUST
+      secure: false, //  MUST
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
       },
+      tls: {
+        rejectUnauthorized: false, // important
+      },
     });
-    console.log("MAIL HOST:", process.env.MAIL_HOST);
-    console.log("MAIL USER:", process.env.MAIL_USER);
 
     let info = await transporter.sendMail({
-      from: `"StudyNotion" <${process.env.MAIL_USER}>`, // better format
+      from: `"StudyNotion" <${process.env.MAIL_USER}>`,
       to: email,
       subject: title,
       html: body,
